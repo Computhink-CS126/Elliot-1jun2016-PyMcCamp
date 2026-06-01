d4930aac4f0a0b7942c2dcd149bf715fe321b47d
Computhink-CS126 <cs126@computhink.com.sg>
2026-06-01 04:38:51 +0000

    Jun 1, 2026, 4:38 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -3,38 +3,70 @@
 print("hello from day1")
 
 ########################################################################
-# Task 1:
+# Task 1:Hello world
 
-
+print("todayisyou")
 
 ########################################################################
 # Task 2:
-
-
-
+print("Elliot")
+print("I am ten")
+print("My school that I attend is catholic high")
+print("If I had $2000 as a reward, I will buy a switch2")
 ########################################################################
 # Task 3:
+animal1 = "tiger" 
 
+animal2 = "cow"
 
+animal3 = "cat"
 
+print(animal1)
+
+print(animal2)
+
+print(animal3)
 ########################################################################
 # Task 4:
+num1 = 10000
 
+num2 = 20000
 
+print(num1 + num2)
 
+print(num1 - num2)
+
+print(num1 * num2)
+
+print(num1 / num2)
 ########################################################################
 # Task 5:
+name = "elliot"
 
+food = "chicken rice"
 
+print("I am " + name + ". I like to eat " + food + ".")
 
 ########################################################################
 # Task 6:
+def intro(): 
+    print("Elliot")
+    print("I am ten")
+    print("My school that I attend is catholic high")
+    print("If I had $2000 as a reward, I will buy a switch2")
+
+intro()
 
 
 
 ########################################################################
 # Task 7:
+def addition(num1,num2):
+    print(num1 + num2)
 
+addition(555,666)
+
+addition(667,236)
 
 
 ########################################################################
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -4,3 +4,88 @@
 
 
 ################## On Chat Commands Section #####################
+def teleport():
+    agent.teleport_to_player()
+
+def move_foward(step):
+    agent.move(FORWARD, step)
+
+def move_back(step):
+    agent.move(BACK, step)
+
+def move_up(step):
+    agent.move(UP, step)
+
+def move_down(step):
+    agent.move(DOWN, step)
+
+
+def turn_left():
+    agent.turn(LEFT)
+
+
+def turn_right():
+    agent.turn(RIGHT)
+
+
+
+player.on_chat("come", teleport)
+player.on_chat("fw", move_foward)
+player.on_chat("bk", move_back)
+player.on_chat("up", move_up)
+player.on_chat("down", move_down)
+player.on_chat("tl", turn_left)
+player.on_chat("tr", turn_right)
+
+
+def move_from_gold():
+    agent.move(FORWARD, 4)
+    agent.turn(LEFT)
+    agent.move(FORWARD, 4)
+    agent.turn(RIGHT)
+    agent.move(FORWARD,4)
+
+
+    
+player.on_chat("q1", move_from_gold)
+
+def move_from_dimond():
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+player.on_chat("w1", move_from_dimond)
+
+def move_from_top():
+    agent.move(FORWARD, 2)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+
+player.on_chat("e1", move_from_top)
+
+def combine_obs():
+    agent.move(FORWARD, 4)
+    agent.turn(LEFT)
+    agent.move(FORWARD, 4)
+    agent.turn(RIGHT)
+    agent.move(FORWARD,4) 
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 2)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+
+player.on_chat("r1", combine_obs)
+

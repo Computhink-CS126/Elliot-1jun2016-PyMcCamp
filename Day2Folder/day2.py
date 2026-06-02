b8156c247011a6272166b529a8fdfe127426be29
Computhink-CS126 <cs126@computhink.com.sg>
2026-06-02 02:22:38 +0000

    Jun 2, 2026, 2:22 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -32,7 +32,7 @@ for count in range(1,11):
 
 ########################################################################
 # Task 5:
-for count in range(1,11):
+for count in range(2.101,2):
     print(count)
 
 

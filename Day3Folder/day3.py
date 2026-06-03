f976da13a58ee83a60e1bb3181d291f9da907ef6
Computhink-CS126 <cs126@computhink.com.sg>
2026-06-03 02:37:42 +0000

    Jun 3, 2026, 2:37 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -29,7 +29,7 @@ print("hello from day3")
 
 ########################################################################
 # Task 4:
-num_item = input("how many apples do you want,each cost $0.5: ") 
+num_item = input("how many apples do you want,each cost $0.5: ") #anything that is type into the terminal via input is string
 total_cost = 0.5 * num_item
 
 print(num_item + " apples cost $" + total_cost)

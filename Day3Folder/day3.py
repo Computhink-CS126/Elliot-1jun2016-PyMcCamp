3d4cae50de0ec59a9dd4a568036cb5cb68647635
Computhink-CS126 <cs126@computhink.com.sg>
2026-06-03 02:33:12 +0000

    Jun 3, 2026, 2:33 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -32,7 +32,7 @@ print("hello from day3")
 num_item = input("how many apples do you want,each cost $1")
 total_cost = 1 * num_item
 
-print()
+print("")
 
 ########################################################################
 # Task 5:

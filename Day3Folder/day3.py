7b29f42c014c592cbe54af0bf26b50280536065a
Computhink-CS126 <cs126@computhink.com.sg>
2026-06-03 02:55:22 +0000

    Jun 3, 2026, 2:55 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -40,7 +40,8 @@ print(str(num_item) + " apples cost $" + str(total_cost))
 age1 = int(input("What is the age of person A"))
 age2 = int(input("What is the age of person b"))
 
-
+''
+''
 
 ########################################################################
 # Task 6:

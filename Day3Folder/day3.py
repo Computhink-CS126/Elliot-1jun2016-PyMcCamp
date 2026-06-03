f8421ac09ae7c9b52d5ee832aabad6c6a112e141
Computhink-CS126 <cs126@computhink.com.sg>
2026-06-03 03:36:43 +0000

    Jun 3, 2026, 3:36 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -117,7 +117,7 @@ print("hello from day3")
 # Task 8:
 import random 
 num1 = random.randint(1,100)
-num2 = 
+
 ########################################################################
 # Additional exercises:
 

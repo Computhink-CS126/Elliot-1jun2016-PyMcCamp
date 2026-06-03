0dca1e9a2b94aead492faf718e889c4d4ecad8ec
Computhink-CS126 <cs126@computhink.com.sg>
2026-06-03 02:25:48 +0000

    Jun 3, 2026, 2:25 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -24,7 +24,7 @@ num2 = int(input("Give me the second number?"))
 
 sum = num1 + num2
 
-print((num1) + " + " +  str(num2) + " = " + str(sum))
+print(str(num1) + " + " +  str(num2) + " = " + str(sum))
 
 
 ########################################################################

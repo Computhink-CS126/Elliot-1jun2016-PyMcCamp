e9f1b88b38e5772a176e55a5bcea26f162ed051f
Computhink-CS126 <cs126@computhink.com.sg>
2026-06-03 03:11:59 +0000

    Jun 3, 2026, 3:11 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -29,26 +29,91 @@ print("hello from day3")
 
 ########################################################################
 # Task 4: 
-num_item =int(input("how many apples do you want,each cost $0.5: ") )
-total_cost = 0.5 * num_item
+# num_item =int(input("how many apples do you want,each cost $0.5: ") )
+# total_cost = 0.5 * num_item
 
-print(str(num_item) + " apples cost $" + str(total_cost))
+# print(str(num_item) + " apples cost $" + str(total_cost))
 
 # [5] apples cost $[5]
 ########################################################################
 # Task 5:
 age1 = int(input("What is the age of person A"))
 age2 = int(input("What is the age of person b"))
-
+# if age1 > age2
 
 ########################################################################
-# Task 6:
+# Task 6:a
 
 
 
 ########################################################################
 # Task 7:
+# Write all your codes for Day 3 here.
+# COMMENT out the previous task before going on to the next task
+print("hello from day3")
 
+########################################################################
+# Task 1:
+# name = input("What is your name?")
+# title = input("What is your title?")
+# command = input("What is your command?")
+
+# print(title + name + "commands the paesants to" + command + "!.")                  
+########################################################################
+# Task 2:
+# name = input("What is your name?")
+# num_pen = int(input("num of pens?")) 
+# print(name + " bought " + str(num_pen) + " pens")
+
+
+
+########################################################################
+# Task 3:
+# num1 = int(input("Give me the first number?"))
+# num2 = int(input("Give me the second number?"))
+#anything that is type into the terminal via input is string
+# sum = num1 + num2
+
+# print(str(num1) + " + " +  str(num2) + " = " + str(sum))
+
+
+########################################################################
+# Task 4: 
+# num_item =int(input("how many apples do you want,each cost $0.5: ") )
+# total_cost = 0.5 * num_item
+
+# print(str(num_item) + " apples cost $" + str(total_cost))
+
+# [5] apples cost $[5]
+########################################################################
+# Task 5:
+age1 = int(input("What is the age of person A"))
+age2 = int(input("What is the age of person b"))
+if age1 > age2:
+    print("personA is older than personB")
+else:
+    print("personA is not older than personB")
+
+########################################################################
+# Task 6:
+real_password = "passme"
+user_password = input("what is the password")
+if real_password == user_password:
+    print("Access Granted!")
+else:
+    print("Access Denied")
+########################################################################
+# Task 7:
+
+
+
+########################################################################
+# Task 8:
+
+
+
+########################################################################
+# Additional exercises:
 
 
 ########################################################################

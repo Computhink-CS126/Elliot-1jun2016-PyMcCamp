de6e48ef3d6ee741e877dab9a24a90d99289c023
Computhink-CS126 <cs126@computhink.com.sg>
2026-06-03 02:04:55 +0000

    Jun 3, 2026, 2:04 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -11,7 +11,9 @@ command = input("What is your command?")
 print(title + name + "commands the paesants to" + command + "!.")                  
 ########################################################################
 # Task 2:
-num_pen = input()           
+num_pen = input(
+    'z'
+)           
 
 
 ########################################################################
